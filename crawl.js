4076c03405d245a057a67bbc77f0ce4f7bca3cc6
Enes Demir <123deli4567@gmail.com>
2026-03-24 23:38:11 +0300

    crwal single page witj error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,29 @@
 import { JSDOM } from 'jsdom';
 
+export async function crawPage(currentURL) {
+  console.log(`actively crwaling ${currentURL}`);
+
+  try {
+    const resp = await fetch(currentURL);
+    if (resp.status > 309) {
+      console.log(
+        `error in fetch with the status code ${resp.status} on page: ${currentURL}`
+      );
+      return;
+    }
+    const contentType = resp.headers.get('content-type');
+    if (!contentType.includes('text/html')) {
+      console.log(
+        `non html response, content type: ${contentType}, on page: ${currentURL}`
+      );
+      return;
+    }
+    console.log(await resp.text());
+  } catch (err) {
+    console.log(`error in fetch  ${err}.message, on page ${currentURL}`);
+  }
+}
+
 export function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
